nitrogen-project/android_hardware_google_gchips

gralloc3: Replace TARGET_SOC with TARGET_BOARD_PLATFORM
TARGET_SOC is vendor-defined, while TARGET_BOARD_PLATFORM is recognized
by the build system. Currently, both are set to the same value.

This change deprecates the former variable in favor of the latter.

Bug: 150506183
Test: No difference in build output file listing after a clean build of
    aosp_slider.
Change-Id: I0687722fb6aac7253eb00dd60768cd9e64af5818

gralloc3/src/Android.mk
@@ -108,7 +108,7 @@ MALI_GRALLOC_DISABLE_ALOGV?=1
 GRALLOC_IMPORT_BUFFER_REQUIRED?=0
 
 # Used for WFD on exynos3830
-ifeq ($(TARGET_SOC), exynos3830)
+ifeq ($(TARGET_BOARD_PLATFORM), exynos3830)
 GRALLOC_ION_HEAP_CRYPTO_MASK:=1
 else
 GRALLOC_ION_HEAP_CRYPTO_MASK:=0
@@ -210,10 +210,10 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
 LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
 ifeq ($(shell expr $(GRALLOC_VERSION_MAJOR) \<= 1), 1)
-    ifeq ($(TARGET_SOC),)
+    ifeq ($(TARGET_BOARD_PLATFORM),)
         LOCAL_MODULE := gralloc.default
     else
-        LOCAL_MODULE := gralloc.$(TARGET_SOC)
+        LOCAL_MODULE := gralloc.$(TARGET_BOARD_PLATFORM)
     endif
 else ifeq ($(GRALLOC_VERSION_MAJOR), 2)
     ifeq ($(GRALLOC_MAPPER), 1)

ExyHyperBrick/android_hardware_samsung_slsi_exynos

gralloc3/src/Android.mk
@@ -108,7 +108,7 @@ MALI_GRALLOC_DISABLE_ALOGV?=1
 GRALLOC_IMPORT_BUFFER_REQUIRED?=0
 
 # Used for WFD on exynos850
-ifeq ($(TARGET_SOC), exynos850)
+ifeq ($(TARGET_BOARD_PLATFORM), exynos850)
 GRALLOC_ION_HEAP_CRYPTO_MASK:=1
 else
 GRALLOC_ION_HEAP_CRYPTO_MASK:=0
@@ -214,10 +214,10 @@ LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
 LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
 ifeq ($(shell expr $(GRALLOC_VERSION_MAJOR) \<= 1), 1)
-    ifeq ($(TARGET_SOC),)
+    ifeq ($(TARGET_BOARD_PLATFORM),)
         LOCAL_MODULE := gralloc.default
     else
-        LOCAL_MODULE := gralloc.$(TARGET_SOC)
+        LOCAL_MODULE := gralloc.$(TARGET_BOARD_PLATFORM)
     endif
 else ifeq ($(GRALLOC_VERSION_MAJOR), 2)
     ifeq ($(GRALLOC_MAPPER), 1)